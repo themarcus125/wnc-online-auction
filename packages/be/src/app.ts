1ed65bef9cac5fe1a2fbe2c59cdcfab1285bc060
QuocLamHong <quoclamhong1312@gmail.com>
2021-10-14 21:19:03 +0700

    fix: BE serve static file route

--- packages/be/src/app.ts
+++ packages/be/src/app.ts
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../public')));
+app.use('/public', express.static(path.join(__dirname, '../public')));
 
 app.use(router);
 app.use('*', (req, res) => {
