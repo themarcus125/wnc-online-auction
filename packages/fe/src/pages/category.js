17b7285f26b7afd1df6050e69ca06b055256a388
QuocLamHong <quoclamhong1312@gmail.com>
2021-10-17 19:44:51 +0700

    fix title for category pages, remove favorite button if product is owned by user

--- packages/fe/src/pages/category.js
+++ packages/fe/src/pages/category.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { Router } from '@reach/router';
 
-import BaseWrapper from '../components/common/Wrapper/BaseWrapper';
 import CategoryPage from '../components/CategoryPage';
 import CategoryProductPage from '../components/CategoryProductPage';
 import CommonLayout from '../components/common/Layout/CommonLayout';
@@ -10,16 +9,8 @@ const CategoryHomePage = () => {
   return (
     <CommonLayout>
       <Router basepath="/category">
-        <BaseWrapper
-          path="/:categoryId"
-          title={'Category'}
-          component={CategoryPage}
-        />
-        <BaseWrapper
-          path="/:categoryId/:subCategoryId"
-          title={'Category'}
-          component={CategoryProductPage}
-        />
+        <CategoryPage path="/:categoryId" />
+        <CategoryProductPage path="/:categoryId/:subCategoryId" />
       </Router>
     </CommonLayout>
   );
