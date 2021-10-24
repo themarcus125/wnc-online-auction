ea242974814f8d7c3ed45b42ec60e145b345df64
Khoa Ngo <ngohakhoa@gmail.com>
2021-10-24 11:31:14 +0700

    Rearrange structure, install eslint and other packages.

--- packages/fe-new/src/App.js
+++ packages/fe-new/src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './assets/images/logo.svg';
+import './assets/css/App.css';
 
 function App() {
   return (
